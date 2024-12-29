0d42b557a83731b000405cc82287d1efc5be85fb
Emilien <emilien.marotta@gmail.com>
2024-12-29 10:40:16 +0100

    Ajust offer padding

--- src/Body/Offer.jsx
+++ src/Body/Offer.jsx
@@ -6,8 +6,8 @@ export default function Offer({
   price,
 }) {
   return (
-    <div className="flex h-auto w-[100%] flex-col items-center justify-between gap-3 rounded-lg bg-white px-3 py-5 text-center shadow-2xl xs:w-[250px] sm:w-[300px] sm:px-8">
-      <img className="h-7 xs:mb-3 xs:h-10" src={imageSrc} alt={altText} />
+    <div className="flex h-auto w-[100%] flex-col items-center justify-between gap-3 rounded-lg bg-white px-8 py-5 text-center shadow-2xl xs:w-[250px] xs:px-3 sm:w-[300px] sm:px-8">
+      <img className="h-9 xs:mb-3 xs:h-12" src={imageSrc} alt={altText} />
       <h3>{title}</h3>
       <p className="text-smTextWhtColor flex-grow">{description}</p>
       <p className="font-semibold">À partir de {price}€ HT</p>
