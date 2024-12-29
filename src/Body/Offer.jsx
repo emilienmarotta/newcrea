a3e0e6fc0dd1d3ddbc77a8a9bef445cdeb41784f
Emilien <emilien.marotta@gmail.com>
2024-12-29 10:52:35 +0100

    Replace violet-200 bg with white bg

--- src/Body/Offer.jsx
+++ src/Body/Offer.jsx
@@ -6,7 +6,7 @@ export default function Offer({
   price,
 }) {
   return (
-    <div className="flex h-auto w-[100%] flex-col items-center justify-between gap-3 rounded-lg bg-white px-8 py-5 text-center shadow-2xl xs:w-[250px] xs:px-3 sm:w-[300px] sm:px-8">
+    <div className="flex h-auto w-[100%] flex-col items-center justify-between gap-3 rounded-lg bg-violet-50 px-8 py-5 text-center shadow-2xl xs:w-[250px] xs:px-3 sm:w-[300px] sm:px-8">
       <img className="h-9 xs:mb-3 xs:h-12" src={imageSrc} alt={altText} />
       <h3>{title}</h3>
       <p className="text-smTextWhtColor flex-grow">{description}</p>
--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -7,7 +7,7 @@ export default function Button({ text, style_ }) {
     );
   } else {
     return (
-      <button className="hover:text-paletteColor3 border-paletteColor1 bg-paletteColor3 text-paletteColor1 hover:bg-paletteColor1 transform rounded-full border-2 px-4 py-2 transition-all duration-300 ease-in-out hover:origin-center hover:scale-105">
+      <button className="hover:text-paletteColor3 border-paletteColor1 text-paletteColor1 hover:bg-paletteColor1 transform rounded-full border-2 bg-white px-4 py-2 transition-all duration-300 ease-in-out hover:origin-center hover:scale-105">
         {text}
       </button>
     );
