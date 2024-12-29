ebf0535282e68933a05e9f97f6f76ecaab65aeca
Emilien <emilien.marotta@gmail.com>
2024-12-29 10:31:46 +0100

    Edit offer pictures size

--- src/Body/Offer.jsx
+++ src/Body/Offer.jsx
@@ -7,7 +7,7 @@ export default function Offer({
 }) {
   return (
     <div className="flex h-auto w-[100%] flex-col items-center justify-between gap-3 rounded-lg bg-white px-3 py-5 text-center shadow-2xl xs:w-[250px] sm:w-[300px] sm:px-8">
-      <img className="mb-3 h-20" src={imageSrc} alt={altText} />
+      <img className="h-7 xs:mb-3 xs:h-10" src={imageSrc} alt={altText} />
       <h3>{title}</h3>
       <p className="text-smTextWhtColor flex-grow">{description}</p>
       <p className="font-semibold">À partir de {price}€ HT</p>
