4126c6b4985f2995b2ad4fc50c489173491b8660
Emilien <emilien.marotta@gmail.com>
2024-12-30 12:20:04 +0100

    Change offer bg color

--- src/Body/Offer.jsx
+++ src/Body/Offer.jsx
@@ -6,12 +6,12 @@ export default function Offer({
   price,
 }) {
   return (
-    <div className="flex h-auto w-[100%] flex-col items-center justify-between gap-3 rounded-lg bg-violet-50 px-8 py-5 text-center shadow-2xl xs:w-[250px] xs:px-3 sm:w-[300px] sm:px-8">
+    <div className="flex h-auto w-[100%] flex-col items-center justify-between gap-3 rounded-lg bg-white px-8 py-5 text-center shadow-2xl xs:w-[250px] xs:px-3 sm:w-[300px] sm:px-8">
       <img className="h-9 xs:mb-3 xs:h-12" src={imageSrc} alt={altText} />
       <h3>{title}</h3>
-      <p className="text-smTextWhtColor flex-grow">{description}</p>
+      <p className="flex-grow text-smTextWhtColor">{description}</p>
       <p className="font-semibold">À partir de {price}€ HT</p>
-      <a className="underline-animation text-paletteColor2 mt-3" href="#">
+      <a className="underline-animation mt-3 text-paletteColor2" href="#">
         En savoir plus
       </a>
     </div>
